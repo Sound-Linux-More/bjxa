5a46e95b991050ea5392dacb9b5d488b284a3682
Dridi Boukelmoune <dridi.boukelmoune@gmail.com>
2018-09-26 13:52:12 +0200

    libbjxa: Move definitions around

--- bjxa.h
+++ bjxa.h
@@ -36,8 +36,9 @@ int bjxa_free_decoder(bjxa_decoder_t **);
 ssize_t bjxa_parse_header(bjxa_decoder_t *, const void *, size_t);
 ssize_t bjxa_fread_header(bjxa_decoder_t *, FILE *);
 
+int bjxa_decode_format(bjxa_decoder_t *, bjxa_format_t *);
+int bjxa_decode(bjxa_decoder_t *, void *, size_t, const void *, size_t);
+
 ssize_t bjxa_dump_riff_header(bjxa_decoder_t *, void *, size_t);
 ssize_t bjxa_fwrite_riff_header(bjxa_decoder_t *, FILE *);
 
-int bjxa_decode_format(bjxa_decoder_t *, bjxa_format_t *);
-int bjxa_decode(bjxa_decoder_t *, void *, size_t, const void *, size_t);
